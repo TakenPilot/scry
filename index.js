c9e9b625debd03d51cae79e92fe9d06bedd019d5
Dane Stuckel <dane.stuckel@gmail.com>
2014-08-26 14:01:49 -0400

    Crop the images

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 var Promise = require('bluebird');
-var util = require('util');
 var Phantom = Promise.promisifyAll(require('node-phantom-simple'));
 var _ = require('lodash');
-var async = Promise.promisifyAll(require('async'));
 var gm = require('gm');
 var compare = Promise.promisify(gm.compare);
 var args = require('minimist')(process.argv.slice(2));
 console.dir(args);
 
+var defaults = _.defaults(args, {
+  base: '.tmp/base.png',
+  new: '.tmp/new.png',
+  diff: '.tmp/diff.png',
+  width: 640,
+  height: 480
+});
 
-/**
- * To use this, you need Cairo for Image manipulation
- * Install dependencies with:
- *     wget https://raw.githubusercontent.com/LearnBoost/node-canvas/master/install -O - | sh
- */
-
-var baseUrl = args[0];
-var newUrl = args[1];
-
-var defaults = {
-  baseFilename: '.tmp/base.png',
-  newFilename: '.tmp/new.png',
-  diffFilename: '.tmp/diff.png'
-};
+function cropFile(filename, width, height) {
+  height = height || 99999;
+  width = width || 99999;
+  var d = Promise.defer();
+  gm(filename).crop(width, height, 0, 0).write(filename, function (err) {
+    if (err) d.reject(err);
+    else d.resolve();
+  });
+  return d.promise;
+}
 
 function renderPage(page, url, filename) {
 
   var open = Promise.promisify(page.open);
+  var evaluate = Promise.promisify(page.evaluate);
   return open(url).then(function (status) {
     console.log("Opening", url, ":", status);
     return page;
@@ -34,24 +36,31 @@ function renderPage(page, url, filename) {
     var d = Promise.defer();
 
     setTimeout(function () {
+      evaluate(function() {
+        document.body.bgColor = 'white';
+      });
       var render = Promise.promisify(page.render);
       console.log('Rendering', url, 'to', filename);
       d.resolve(render(filename));
-    }, 1000);
+    }, 3000);
 
     return d.promise;
   });
 }
 
 function compareUrls(baseUrl, newUrl) {
-  var baseFilename = defaults.baseFilename,
-      newFilename = defaults.newFilename,
-      diffFilename = defaults.diffFilename;
+  var baseFilename = defaults.base,
+      newFilename = defaults.new,
+      diffFilename = defaults.diff;
 
 
   Phantom.createAsync().then(function (phantom) {
     var createPage = Promise.promisify(phantom.createPage);
     return createPage().then(function (page) {
+      page.set('viewportSize', {
+        width: defaults.width,
+        height: defaults.height
+      });
       return renderPage(page, baseUrl, baseFilename).then(function () {
         return page;
       });
@@ -71,13 +80,35 @@ function compareUrls(baseUrl, newUrl) {
       });
       return d.promise;
     }).then(function () {
+      var d = Promise.defer();
+      /** If they want to compare them beside each other */
+      if (args.beside) {
+        gm(baseFilename)
+            .append(newFilename, true)
+            .append(diffFilename, true)
+            .write(diffFilename, function (err) {
+              if (err) throw err;
+              d.resolve();
+            });
+      }
+      return d.promise;
+    }).then(function () {
+      var top;
+      /** If they recommend a top, crop all output */
+      if (args.top) {
+        top = parseInt(args.top, 10);
+        return Promise.all([
+          cropFile(diffFilename, null, top),
+          cropFile(baseFilename, null, top),
+          cropFile(newFilename, null, top)
+        ]);
+      }
+      return Promise.resolve();
+    }).then(function () {
       phantom.exit();
     });
   });
 }
 
-compareUrls(
-    'http://www.nymag.com/daily/intelligencer/2013/12/city-would-like-to-hear-the-real-nypd-tapes.html',
-    'http://factory.qa.nymetro.com/daily/intelligencer/2013/12/city-would-like-to-hear-the-real-nypd-tapes.html'
-);
+compareUrls(args._[0], args._[1]);
 
